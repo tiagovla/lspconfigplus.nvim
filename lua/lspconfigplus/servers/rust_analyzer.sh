ff57534f8fb2299e23b96ec3a98f447d1aa9095d
tiagovla <tiagovla@gmail.com>
2021-08-16 18:14:34 -0300

    Add rust analyzer language server

---
+++ lua/lspconfigplus/servers/rust_analyzer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+os=$(uname -s | tr "[:upper:]" "[:lower:]")
+case $os in
+	linux)
+		platform="linux"
+		;;
+	darwin)
+		platform="mac"
+		;;
+esac
+
+case $1 in
+	install)
+		curl -L -o "rust-analyzer-$platform" "https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-$platform"
+		mv rust-analyzer-$platform rust-analyzer
+		chmod +x rust-analyzer
+		;;
+
+	update)
+		echo not implemented yet
+		;;
+
+	uninstall)
+		echo not implemented yet
+		;;
+
+	*)
+		exit 1
+		;;
+
+esac
